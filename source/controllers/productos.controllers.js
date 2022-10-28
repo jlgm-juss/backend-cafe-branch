4bbf706bd8bfc045f6aa0b6e46a8a8dcc626da01
JLGM <juss.jlgm@gmail.com>
2022-10-28 00:26:53 -0300

    agregamos la logica para crear un producto y obtener un arreglo de todos los productos

--- source/controllers/productos.controllers.js
+++ source/controllers/productos.controllers.js
@@ -1,11 +1,38 @@
-export const listarProductos = (req, res) => {
-  res.send("Aqui tengo que retornar un arreglo de productos");
+import Producto from "../models/producto";
+
+export const listarProductos = async (req, res) => {
+
+  try{
+    //buscar todos los productos en la BD
+    const listaProductos = await Producto.find();       //<------ .find para que encuentre uno o todos los arreglos de objetos. (finde es una querie). además le puedo agregar parámetros si necesito.
+    //responder al usuario que todo salio bien
+    res.status(200).json(listaProductos)
+  }catch(error){
+    console.log(error);
+    res.status(404).json({
+      mensaje: 'Error al intentar buscar los productos'
+    })
+  }
+
 };
 
-export const crearProducto = (req, res) => {
-  //extraer del body los datos
-  console.log(req.body)
-  //agregar la validacion correspondiente
-  //guardar ese producto en la BD
-  res.send("Esto es una prueba de la peticion GET");
+export const crearProducto = async (req, res) => {
+  try{
+    //extraer del body los datos
+    console.log(req.body)
+    //agregar la validacion correspondiente
+    const productoNuevo = new Producto(req.body)
+    //guardar ese producto en la BD
+await productoNuevo.save();
+
+    //responder al usuario que todo salio bien
+    res.status(201).json({
+      mensaje: 'El producto fue correctamente creado'
+    })
+  }catch(error){
+    console.log(error);
+    res.status(400).json({
+      mensaje: 'Error al intentar agregar un producto'
+    })
+  }
 };
